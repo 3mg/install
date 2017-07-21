20735acc2f416bbaea8aa30db55fc70844631d4b
Nickolay Savchenko <3mg@bk.ru>
2017-07-21 19:20:02 +0300

    add arm 64

--- install.sh
+++ install.sh
@@ -61,6 +61,7 @@ start() {
     MINGW*) os=windows ;;
   esac
   case "$arch" in
+    *aarch64*) arch=aarch64 ;;
     *armv6l*) arch=armv6l ;;
     *armv7l*) arch=armv7l ;;
     *x86_64*) arch=x64 ;;
